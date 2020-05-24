919923ea40d7616130ad018a1efc6a8cf09535bd
Arif F Lazuardi <lazuardi.dp@gmail.com>
2020-05-24 10:20:11 +0700

    agar id rumah tangga bisa campuran huruf dan angka

--- donjo-app/models/Rtm_model.php
+++ donjo-app/models/Rtm_model.php
@@ -203,7 +203,7 @@
 	public function delete($no_kk='', $semua=false)
 	{
 		if (!$semua) $this->session->success = 1;
-		
+
 		$temp['id_rtm'] = 0;
 		$temp['rtm_level'] = 0;
 		$temp['updated_at'] = date('Y-m-d H:i:s');
@@ -327,7 +327,7 @@
 		$sql = "SELECT p.id, p.nik, p.nama, h.nama as kk_level
 			FROM tweb_penduduk p
 			LEFT JOIN tweb_penduduk_hubungan h ON p.kk_level = h.id
-			WHERE (status = 1 OR status = 3) AND status_dasar = 1 AND (id_rtm = 0 OR id_rtm IS NULL)";
+			WHERE (status = 1 OR status = 3) AND status_dasar = 1 AND (id_rtm = '0' OR id_rtm IS NULL)";
 		$query = $this->db->query($sql);
 		$data = $query->result_array();
 
