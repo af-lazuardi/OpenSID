c580f8a681c87aa99c4366f5690139254f697546
Arif F Lazuardi <lazuardi.dp@gmail.com>
2020-06-06 01:42:29 +0700

    commit dari user A

--- desa-contoh/config/config.php
+++ desa-contoh/config/config.php
@@ -27,7 +27,7 @@
 	Kode provinsi sesuai dengan yg di http://pusatkrisis.kemkes.go.id/daftar-kode-provinsi
 */
 	$config['covid_data'] = 1;
-	$config['provinsi_covid'] = 51; // kode provinsi. Comment baris ini untuk menampilkan data Indonesia
+	$config['provinsi_covid'] = 60; // kode provinsi. Comment baris ini untuk menampilkan data Indonesia
 	$config['covid_desa'] = 1; // Tampilkan status Covid-19 dari data OpenSID desa
 	$config['covid_rss'] = 1; // Tampilkan rss feed https://www.covid19.go.id/feed/ di halaman muka
 
