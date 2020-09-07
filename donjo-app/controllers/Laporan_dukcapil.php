27fcf23e1d28c38954802584bed985f243be147a
Arif Lazuardi <lazuardi.dp@gmail.com>
2020-09-07 13:20:41 +0700

    view laporan dukcapil udah okay

--- donjo-app/controllers/Laporan_dukcapil.php
+++ donjo-app/controllers/Laporan_dukcapil.php
@@ -1,14 +1,13 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class Laporan_dukcapil extends CI_Controller {
+class Laporan_dukcapil extends Admin_Controller {
 
 	public function __construct()
 	{
 		parent::__construct();
-		session_start();
-		$this->load->model('user_model');
-		$this->load->model('laporan_penduduk_model');
-		$this->load->model('laporan_dukcapil_model');
+
+		$this->load->model(['user_model', 'laporan_penduduk_model', 'laporan_dukcapil_model', 'header_model']);
+
 		$grup = $this->user_model->sesi_grup($_SESSION['sesi']);
 		if ($grup != 1 AND $grup != 2 AND $grup != 3)
 		{
@@ -18,10 +17,10 @@ class Laporan_dukcapil extends CI_Controller {
 				unset($_SESSION['request_uri']);
 			redirect('siteman');
 		}
-		$this->load->model('header_model');
-		$_SESSION['per_page'] = 500;
-		$this->modul_ini = 3;
-	
+
+		$this->_set_page = ['50', '100', '200'];
+		$this->_header = $this->header_model->get_data();
+		$this->modul_ini = 904;
 	}
 
 	public function index($kateg=0)
@@ -30,26 +29,29 @@ class Laporan_dukcapil extends CI_Controller {
 	}
 
 	public function pindah() {
+		$this->sub_modul_ini = 905;
 		$this->loadView("pindah");
 	}
 
 	public function datang() {
+		$this->sub_modul_ini = 906;
 		$this->loadView("datang");
 	}
 
 	public function lahir() {
+		$this->sub_modul_ini = 907;
 		$this->loadView("lahir");
 	}
 
 	public function mati() {
+		$this->sub_modul_ini = 908;
 		$this->loadView("mati");
 	}
-
-
 
 	private function loadView($kateg=0)
 	{
 		
+
 	 	$kategori = $kateg;
 		$startDate =  $_POST['startDate'];
 		$endDate =  $_POST['endDate'];
@@ -60,16 +62,11 @@ class Laporan_dukcapil extends CI_Controller {
 		}
 		$exportDate = ["startDate"=> $startDate, "endDate"=>$endDate];
 		$agregat=["jenis" => $kategori, "content"=> json_decode($data), "export_date"=>$exportDate];
-		$nav['act'] = 3;
-		$nav['act_sub'] = 27;
-		$header = $this->header_model->get_data();
-		$this->load->view('header', $header);
-		$this->load->view('nav', $nav);
+
+		$this->load->view('header', $this->_header);
+		$this->load->view('nav');
 		$this->load->view("laporan_dukcapil/".$kategori, $agregat);
 		$this->load->view('footer');
-		
-		
-		
 	}
 
 	public function cetak($jenis=0, $startDate=0, $endDate=0)
