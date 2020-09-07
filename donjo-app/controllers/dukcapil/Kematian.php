e7d3d5f244f72d66ad0e50bf7be814fd3caf32d6
Arif Lazuardi <lazuardi.dp@gmail.com>
2020-09-07 13:36:28 +0700

    layanan dukcapil okay

--- donjo-app/controllers/dukcapil/Kematian.php
+++ donjo-app/controllers/dukcapil/Kematian.php
@@ -1,15 +1,14 @@
 <?php if(!defined('BASEPATH')) exit('No direct script access allowed');
 
-class Kematian extends CI_Controller {
+class Kematian extends Admin_Controller {
 
     public function __construct()
 	{
         parent::__construct();
-        session_start();
-		$this->load->model('user_model');
-		$this->load->model('laporan_penduduk_model');
-		$this->load->model('program_bantuan_model');
-		$this->load->model('agregat_dukcapil_model');
+
+        $this->load->model(['user_model', 'laporan_penduduk_model', 'program_bantuan_model', 'agregat_dukcapil_model', 'header_model']);
+
+		/*
 		$grup = $this->user_model->sesi_grup($_SESSION['sesi']);
 		if ($grup != 1 AND $grup != 2 AND $grup != 3)
 		{
@@ -18,19 +17,18 @@ class Kematian extends CI_Controller {
 			else
 				unset($_SESSION['request_uri']);
 			redirect('siteman');
-		}
-		$this->load->model('header_model');
-		$_SESSION['per_page'] = 500;
-		$this->modul_ini = 3;
+		}*/
+
+		$this->_set_page = ['50', '100', '200'];
+		$this->_header = $this->header_model->get_data();
+		$this->modul_ini = 901;
     }
 
     public function index() {
-        $nav['act'] = 4;
-		$nav['act_sub'] = 33;
-		$header = $this->header_model->get_data();
+        $this->sub_modul_ini = 903;
 
-		$this->load->view('header', $header);
-		$this->load->view('nav', $nav);
+		$this->load->view('header', $this->_header);
+		$this->load->view('nav');
 		$this->load->view('dukcapil/kematian');
 		$this->load->view('footer');
     }
